bc77a95f4eed30715e1f4a2a3c6cd9e261562714
roopdinesh <kotaroopdinesh@gmail.com>
2025-08-28 12:33:38 +0530

    pythonlab completed

---
+++ practicesum.py
@@ -0,0 +1,10 @@
+print("Welcome to Python!")
+print("Sum:", 5 + 3)
+print("Hello", "World", "!")
+name = "Alice"
+print("Name:", name)
+name, age, city = "Alice", 22, "New York"
+print("Name:", name, "Age:", age, "City:", city)
+
+
+
---
+++ pythonlab.py
@@ -0,0 +1,25 @@
+'''name = input("Enter your name: ")
+print("Hello,", name)
+city = input("Enter your city: ")
+print("You are from", city)
+age = int(input("Enter your age: "))
+print("You are", age, "years old.")
+rate = float(input("Enter your rate: "))
+print("Your rate is", rate)'''
+
+
+print("welcome to python class")
+
+x=str(input('enter your name:'))
+print('name is', x)
+city=input('enter your city:')
+print('city is', city )
+age=int(input('enter your age:'))
+print('age is', age)                                
+rate=float(input('enter your rate:'))
+print('rate is', rate)      
+
+print(f" name is {x},city is {city},age is {age},rate is {rate}")
+
+print("name is",'dinesh',"city is","nellore","age is",20,"rate is",5.50)
+print(f"{x=},{city=},{age=},{rate=}")
